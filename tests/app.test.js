1db743f7c2b0c84f4a9a0426d15e7cee6fc58231
Jongmin Kim <jmkim990226@gmail.com>
2025-11-24 14:09:44 +0900

    fix: Initial commit

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,11 +3,11 @@ const express = require('express');
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello, World!);
+  res.send('Hello, World!');
 });
 
 describe('GET /', () => {
-  if('should return Hello, World!', async () => {
+  it('should return Hello, World!', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
     expect(res.text).toEqual('Hello, World!');
